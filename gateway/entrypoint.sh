b40b2675852a928ea46545f61da66398234dd69c
viziadam <vizi.adam913@gmail.com>
2026-02-03 10:18:29 +0100

    fixxing image saving, load etc...

--- gateway/entrypoint.sh
+++ gateway/entrypoint.sh
@@ -17,3 +17,44 @@ envsubst '$FRONTEND_UPSTREAM $ADMIN_UPSTREAM $BACKEND_UPSTREAM $PORT' \
   > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
+
+
+# server {
+#   listen 8080;
+#   server_name _;
+
+#   client_max_body_size 50m;
+
+#   location /api/ {
+#     proxy_pass ${BACKEND_UPSTREAM};
+#     proxy_http_version 1.1;
+#     proxy_set_header Host $proxy_host;
+#     proxy_set_header X-Forwarded-Host $host;
+#     proxy_set_header X-Forwarded-Proto $scheme;
+#     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+#     proxy_ssl_server_name on;
+#     proxy_ssl_name $proxy_host;
+#   }
+
+#   location /admin/ {
+#     proxy_pass ${ADMIN_UPSTREAM}/;
+#     proxy_http_version 1.1;
+#     proxy_set_header Host $proxy_host;
+#     proxy_set_header X-Forwarded-Host $host;
+#     proxy_set_header X-Forwarded-Proto $scheme;
+#     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+#     proxy_ssl_server_name on;
+#     proxy_ssl_name $proxy_host;
+#   }
+
+#   location / {
+#     proxy_pass ${FRONTEND_UPSTREAM};
+#     proxy_http_version 1.1;
+#     proxy_set_header Host $proxy_host;
+#     proxy_set_header X-Forwarded-Host $host;
+#     proxy_set_header X-Forwarded-Proto $scheme;
+#     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+#     proxy_ssl_server_name on;
+#     proxy_ssl_name $proxy_host;
+#   }
+# }
