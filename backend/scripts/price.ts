8309b5ba2c19f4a3bcedf1fb9ed119b9d4318cd9
viziadam <vizi.adam913@gmail.com>
2026-03-02 16:50:10 +0100

    some changes

--- backend/scripts/price.ts
+++ backend/scripts/price.ts
@@ -11,14 +11,14 @@ if (
 
   for (const car of cars) {
     if (('price' in car) && car.price) {
-      car.dailyPrice = Number(car.price)
-      car.discountedDailyPrice = null
-      car.biWeeklyPrice = null
-      car.discountedBiWeeklyPrice = null
-      car.weeklyPrice = null
-      car.discountedWeeklyPrice = null
+      car.dailyPrice1_3 = Number(car.price)
+      car.dailyPrice4_6 = 0
+      // car.biWeeklyPrice = null
+      // car.discountedBiWeeklyPrice = null
+      car.weeklyPrice1_2 = null
+      car.weeklyPrice3_4 = null
       car.monthlyPrice = null
-      car.discountedMonthlyPrice = null
+      // car.discountedMonthlyPrice = null
       car.price = undefined
       await car.save()
       logger.info(`${car.id} affected`)
