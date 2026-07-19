769b6ed97630956ba493d3e39c09abcc7471a08a
viziadam <113590667+viziadam@users.noreply.github.com>
2026-07-19 09:22:29 +0200

    fix(backend): route bulk status updates through consistent controller

--- backend/src/routes/bookingRoutes.ts
+++ backend/src/routes/bookingRoutes.ts
@@ -2,13 +2,14 @@ import express from 'express'
 import routeNames from '../config/bookingRoutes.config'
 import authJwt from '../middlewares/authJwt'
 import * as bookingController from '../controllers/bookingController'
+import * as bookingStatusController from '../controllers/bookingStatusController'
 
 const routes = express.Router()
 
 routes.route(routeNames.create).post(authJwt.verifyToken, bookingController.create)
 routes.route(routeNames.checkout).post(bookingController.checkout)
 routes.route(routeNames.update).put(authJwt.verifyToken, bookingController.update)
-routes.route(routeNames.updateStatus).post(authJwt.verifyToken, bookingController.updateStatus)
+routes.route(routeNames.updateStatus).post(authJwt.verifyToken, bookingStatusController.updateStatus)
 routes.route(routeNames.delete).post(authJwt.verifyToken, bookingController.deleteBookings)
 routes.route(routeNames.deleteTempBooking).delete(bookingController.deleteTempBooking)
 routes.route(routeNames.getBooking).get(bookingController.getBooking)
